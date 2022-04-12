ff20613a68d76a7733de2c0b300e4b1cad3b2adc
sahilur12 <87418854+sahilur12@users.noreply.github.com>
2022-04-13 01:02:00 +0600

    code

--- index.php
+++ index.php
@@ -10,10 +10,88 @@
             $_SESSION['system'][$key] = $value;
         }
     ob_end_flush();
+    include('header.php');
 
 	
     ?>
 
+    <style>
+      #main-field{
+        margin-top: 5rem!important;
+      }
+      .cart-img {
+          width: calc(20%);
+          height: 13vh;
+          padding: 3px
+      }
+      .cart-img img{
+        width: 100%;
+        height: 100%;
+      }
+      .cart-qty {
+        font-size: 14px
+      }
+    </style>
+    <body id="page-top">
+        <!-- Navigation-->
+        <div class="toast" id="alert_toast" role="alert" aria-live="assertive" aria-atomic="true">
+        <div class="toast-body text-white">
+        </div>
+      </div>
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand js-scroll-trigger" href="./"><?php echo $_SESSION['system']['name'] ?></a>
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto my-2 my-lg-0">
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.php?page=home">Home</a></li>
+                        <?php if(isset($_SESSION['login_id'])): ?>
+
+
+                        <li class="nav-item dropdown">
+                          <a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="true">
+                            <div class="badge badge-danger cart-count">0</div>
+                            <i class="fa fa-shopping-cart"></i>
+                            <span>Cart</span>
+                          </a>
+                          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton" style="width:25vw">
+                            <div class="cart-list w-100" id="cart_product">
+                            
+                              
+                            </div>
+                            
+                            <div class="d-flex bg-light justify-content-center w-100 p-2">
+                                <a href="index.php?page=cart" class="btn btn-sm btn-primary btn-block col-sm-4 text-white"><i class="fa fa-edit"></i>  View Cart</a>
+                            </div>
+                          </div>
+                          </li>
+                      <?php endif; ?>
+                          
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.php?page=about">About</a></li>
+                        <?php if(isset($_SESSION['login_id'])): ?>
+                       <div class=" dropdown mr-4">
+                            <a href="#" class="text-white dropdown-toggle"  id="account_settings" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?php echo $_SESSION['login_name'] ?> </a>
+                              <div class="dropdown-menu" aria-labelledby="account_settings" style="left: -2.5em;">
+                                <a class="dropdown-item" href="javascript:void(0)" id="manage_my_account"><i class="fa fa-cog"></i> Manage Account</a>
+                                <a class="dropdown-item" href="admin/ajax.php?action=logout2"><i class="fa fa-power-off"></i> Logout</a>
+                              </div>
+                        </div>
+                      <?php else: ?>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="javascript:void(0)" id="login_now">Login</a></li>
+                      <?php endif; ?>
+                       
+                        
+                     
+                    </ul>
+                </div>
+            </div>
+        </nav>
+  <main id="main-field">
+        <?php 
+        $page = isset($_GET['page']) ? $_GET['page'] : 'home';
+        include $page.'.php';
+        ?>
+       
 </main>
 <div class="modal fade" id="confirm_modal" role='dialog'>
     <div class="modal-dialog modal-md" role="document">
@@ -107,4 +185,5 @@
       })
     </script>
     <?php $conn->close() ?>
-    </html>
+
+</html>
